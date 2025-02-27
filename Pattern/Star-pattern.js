29bcb8063d92d28cd2026cf0cd43fea1af7da249
MRiDuL-ICE <abdulwahab22400@gmail.com>
2025-02-27 10:54:59 +0600

    pattern problem

---
+++ Pattern/Star-pattern.js
@@ -0,0 +1,13 @@
+// {
+//   const printPattern1 = () => {
+//     let n = prompt("Plaese enter a number", 4);
+//     for (let i = 0; i < n; i++) {
+//       for (let j = 0; j < n; j++) {
+//         console.log("*");
+//       }
+//       console.log("\n");
+//     }
+//   };
+
+//   printPattern1();
+// }
